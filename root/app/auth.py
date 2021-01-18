e7b261f2c4c20a864b9e97bcdafdcac1153f9f54
wiserain <mail275@gmail.com>
2021-01-18 14:10:45 +0900

    use hashed password for basicauth (#4)

--- root/app/auth.py
+++ root/app/auth.py
@@ -1,5 +1,6 @@
 import os
 from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 basic_auth = HTTPBasicAuth()
 
@@ -8,7 +9,7 @@ AUTH_DATA = {}
 authuser = os.getenv('YTBDL_SERVER_USER', '')
 authpass = os.getenv('YTBDL_SERVER_PASS', '')
 if authuser and authpass:
-    AUTH_DATA.update({authuser: authpass})
+    AUTH_DATA.update({authuser: generate_password_hash(authpass)})
 
 
 @basic_auth.verify_password
@@ -17,4 +18,5 @@ def verify(username, password):
         return True
     if not (username and password):
         return False
-    return AUTH_DATA.get(username) == password
+    if username in AUTH_DATA and check_password_hash(AUTH_DATA.get(username), password):
+        return username
